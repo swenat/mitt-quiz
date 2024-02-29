2771651cc6dfcd4b9e96362c3e9b8639efede77c
Nathalie <lysefeldt@outlook.com>
2024-02-29 21:14:46 +0100

    comments

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -6,26 +6,28 @@ const TimerContainer = styled.div`
 	margin-top: 20px;
 `;
 
+// Functional component Timer
 const Timer: React.FC<{ duration: number; onTimeout: () => void }> = ({
 	duration,
 	onTimeout,
 }) => {
+	// State to track the remaining seconds
 	const [seconds, setSeconds] = useState(duration);
-
+	// useEffect updates every second and clears the interval when the Timer is no longer in use
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setSeconds((prevSeconds) => prevSeconds - 1);
 		}, 1000);
-
+		//Cleanup function
 		return () => clearInterval(interval);
 	}, []);
-
+	//Triggers onTimeOut callback when seconds reach 0
 	useEffect(() => {
 		if (seconds === 0) {
 			onTimeout();
 		}
 	}, [seconds, onTimeout]);
-
+	// Format remaining time as "mm:ss"
 	const formattedTime = `${Math.floor(seconds / 60)}:${(
 		seconds % 60
 	).toLocaleString("en-US", {
